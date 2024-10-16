cd12d32c0f13e6a1990ddae395403da68e023c3f
Ulf Bjorkengren <ulfbjn@gmail.com>
2024-10-16 17:47:43 +0200

    Make file update to support the validate keyword.
    Signed-off-by: Ulf Bjorkengren <ulfbjn@gmail.com>

--- spec/trees/make.sh
+++ spec/trees/make.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
-make -C ../../ $1 VSPECROOT=$2
-#make -C ../../ $1 VSPECROOT=./spec/trees/$2
+make -C ../../ $1 VSPECROOT=$2 VALIDATE='-e validate'
+#make -C ../../ $1 VSPECROOT=$2
+
